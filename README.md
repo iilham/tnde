06e4d4f5f1eb6cd2dc50512570a3da7c4de38949
iilham <muh.ilhamm@gmail.com>
2017-01-27 15:52:00 +0800

    versi1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
+<<<<<<< HEAD
+Yii 2 Advanced Project Template Untuk Tata Naskah Dinas Elekronik
+===============================
+Masih dalam Pengembangan Gan...
+
+DIRECTORY STRUCTURE
+-------------------
+
+```
+common
+    config/              contains shared configurations
+    mail/                contains view files for e-mails
+    models/              contains model classes used in both backend and frontend
+    tests/               contains tests for common classes    
+console
+    config/              contains console configurations
+    controllers/         contains console controllers (commands)
+    migrations/          contains database migrations
+    models/              contains console-specific model classes
+    runtime/             contains files generated during runtime
+backend
+    assets/              contains application assets such as JavaScript and CSS
+    config/              contains backend configurations
+    controllers/         contains Web controller classes
+    models/              contains backend-specific model classes
+    runtime/             contains files generated during runtime
+    tests/               contains tests for backend application    
+    views/               contains view files for the Web application
+    web/                 contains the entry script and Web resources
+frontend
+    assets/              contains application assets such as JavaScript and CSS
+    config/              contains frontend configurations
+    controllers/         contains Web controller classes
+    models/              contains frontend-specific model classes
+    runtime/             contains files generated during runtime
+    tests/               contains tests for frontend application
+    views/               contains view files for the Web application
+    web/                 contains the entry script and Web resources
+    widgets/             contains frontend widgets
+vendor/                  contains dependent 3rd-party packages
+environments/            contains environment-based overrides
+```
+=======
 # tnde
-tata naskah dinas elektronik
+tata naskah data elektronik
+>>>>>>> e39722b1cc6d1a49c0d4b736fd6e4abb63c2b226
---
+++ backend/views/layouts/left.php
@@ -0,0 +1,113 @@
+<aside class="main-sidebar">
+
+    <section class="sidebar">
+
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="<?= $directoryAsset ?>/img/avatar5.png" class="img-circle" alt="User Image"/>
+            </div>
+            <div class="pull-left info">
+                <p>
+                    <?php
+                    if (Yii::$app->user->isGuest) {
+                        
+                    } else {
+                        echo Yii::$app->user->identity->username;
+                    }
+                    ?>
+                </p>
+
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+
+        <!-- search form -->
+        <!--        <form action="#" method="get" class="sidebar-form">
+                    <div class="input-group">
+                        <input type="text" name="q" class="form-control" placeholder="Search..."/>
+                      <span class="input-group-btn">
+                        <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
+                        </button>
+                      </span>
+                    </div>
+                </form>-->
+        <!-- /.search form -->
+
+        <?=
+        dmstr\widgets\Menu::widget(
+                [
+                    'options' => ['class' => 'sidebar-menu'],
+                    'items' => [
+                        ['label' => 'Menu', 'options' => ['class' => 'header']],
+                        ['label' => 'Dashboard', 'icon' => 'fa fa-dashboard', 'url' => ['/site']],
+                        ['label' => 'Surat Masuk', 'icon' => 'fa fa-envelope-open', 'url' => ['/site']],
+                        ['label' => 'Surat Keluar', 'icon' => 'fa fa-envelope', 'url' => ['/site']],
+                        ['label' => 'Surat Disposisi', 'icon' => 'fa fa-paper-plane', 'url' => ['/site']],
+                        [
+                            'label' => 'Template Surat',
+                            'icon' => 'fa fa-book',
+                            'url' => '#',
+                            'items' => [
+                                ['label' => 'Surat Dinas', 'icon' => 'fa fa-circle', 'url' => ['/login'],],
+                                ['label' => 'Sutar Tugas', 'icon' => 'fa fa-circle', 'url' => ['/login'],],
+                                ['label' => 'Surat Perjadin', 'icon' => 'fa fa-circle', 'url' => ['/login'],],
+                            ],
+                        ],
+                        [
+                            'label' => 'Master',
+                            'icon' => 'fa fa-cog',
+                            'url' => '#',
+                            'items' => [
+                                ['label' => 'Satuan Kerja', 'icon' => 'fa fa-circle', 'url' => ['/login'],],
+                                ['label' => 'Subject Matter', 'icon' => 'fa fa-circle', 'url' => ['/login'],],
+                                ['label' => 'Dasar hukum', 'icon' => 'fa fa-circle', 'url' => ['/login'],],
+                                ['label' => 'Pembebanan Anggaran', 'icon' => 'fa fa-circle', 'url' => ['/login'],],
+                            ],
+                        ],
+                        [
+                            'label' => 'CKP Online',
+                            'icon' => 'fa fa-desktop',
+                            'url' => '#',
+                            'items' => [
+                                ['label' => 'CKP Target', 'icon' => 'fa fa-circle', 'url' => ['/login'],],
+                                ['label' => 'CKP Realisasi', 'icon' => 'fa fa-circle', 'url' => ['/login'],],
+                            ],
+                        ],
+                        ['label' => 'Pegawai', 'icon' => 'fa fa-users fa-lg', 'url' => ['/site']],
+                        ['label' => 'Atur Pengguna', 'icon' => 'fa fa-users', 'url' => ['/site']],
+//                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+//                    [
+//                        'label' => 'Same tools',
+//                        'icon' => 'fa fa-share',
+//                        'url' => '#',
+//                        'items' => [
+//                            ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/site'],],
+//                            ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/site'],],
+//                            [
+//                                'label' => 'Level One',
+//                                'icon' => 'fa fa-circle-o',
+//                                'url' => '#',
+//                                'items' => [
+//                                    ['label' => 'Level Two', 'icon' => 'fa fa-circle-o', 'url' => '#',],
+//                                    [
+//                                        'label' => 'Level Two',
+//                                        'icon' => 'fa fa-circle-o',
+//                                        'url' => '#',
+//                                        'items' => [
+//                                            ['label' => 'Level Three', 'icon' => 'fa fa-circle-o', 'url' => '#',],
+//                                            ['label' => 'Level Three', 'icon' => 'fa fa-circle-o', 'url' => '#',],
+//                                        ],
+//                                    ],
+//                                ],
+//                            ],
+//                        ],
+//                    ],
+                    ],
+                ]
+        )
+        ?>
+
+    </section>
+
+</aside>
